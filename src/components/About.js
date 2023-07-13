f30f39bf33c6ea22fcd8047ef756058613ef0c12
김동현 <dtc03003@naver.com>
2023-07-13 18:54:35 +0900

    about 위치 조정

--- src/components/About.js
+++ src/components/About.js
@@ -10,16 +10,19 @@ const About = () => {
                         <h1>About Me</h1>
                     </div>
                     <div className={style.photo}></div>
-                    <div className={style.name}>
+                    <div className={style.char}>
                         <h1>김동현</h1>
                     </div>
-                    <div className={style.birthday}>
+                    <div className={style.char}>
                         <h1>96.03.17</h1>
                     </div>
                 </div>
                 <div className={style.intro}>
                     <div className={style.title}>
                         <h1>자기소개</h1>
+                    </div>
+                    <div className={style.info}>
+                        <h1>대충 자기소개하는 내용</h1>
                     </div>
                 </div>
             </div>
