70bd6f62411786c5ca3cd8fc39d7fe8149d9b9f0
김동현 <dtc03003@naver.com>
2023-07-14 22:48:45 +0900

    스킬페이지 세팅

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,7 +4,39 @@ import style from "./../styles/Skills.module.css";
 const Skills = () => {
     return (
         <div className={style.Skills}>
-            <h1>스킬 나열할 곳</h1>
+            <div className={style.inner}>
+                <h1>📚Tech stack📚</h1>
+                <div className={style.Main}>
+                    <h1>📕Used as the main</h1>
+                    <div className={style.skills}>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                    </div>
+                </div>
+                <div className={style.Sub}>
+                    <h1>📒Used at least once</h1>
+                    <div className={style.skills}>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                        <div className={style.skill}></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
