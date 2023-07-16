b6a921f72ec90409275c71e2212c78a33e8f302d
김동현 <dtc03003@naver.com>
2023-07-16 14:37:52 +0900

    스킬목록 기록, 위치, 크기조정

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,31 +9,30 @@ const Skills = () => {
                 <div className={style.Main}>
                     <h1>📕Used as the main</h1>
                     <div className={style.skills}>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
+                        <div className={style.skill}>html</div>
+                        <div className={style.skill}>css3</div>
+                        <div className={style.skill}>javascript</div>
+                        <div className={style.skill}>react</div>
+                        <div className={style.skill}>java</div>
+                        <div className={style.skill}>github</div>
+                        <div className={style.skill}>markdown</div>
                     </div>
                 </div>
                 <div className={style.Sub}>
                     <h1>📒Used at least once</h1>
                     <div className={style.skills}>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
-                        <div className={style.skill}></div>
+                        <div className={style.skill}>vue.js</div>
+                        <div className={style.skill}>redux</div>
+                        <div className={style.skill}>spring</div>
+                        <div className={style.skill}>spring_boot</div>
+                        <div className={style.skill}>Node.js</div>
+                        <div className={style.skill}>Koa</div>
+                        <div className={style.skill}>express.js</div>
+                        <div className={style.skill}>mysql</div>
+                        <div className={style.skill}>mongodb</div>
+                        <div className={style.skill}>amazon ec2</div>
+                        <div className={style.skill}>ubuntu</div>
+                        <div className={style.skill}>jira</div>
                     </div>
                 </div>
             </div>
