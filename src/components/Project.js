e931ec1c6e51f628207cffcd23da60107625db91
김동현 <dtc03003@naver.com>
2023-07-17 19:25:25 +0900

    project 배치

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,17 @@ import style from "./../styles/Project.module.css";
 const Project = () => {
     return (
         <div className={style.Project}>
-            <h1>프로젝트 나열</h1>
+            <div className={style.inner}>
+                <h1>My Project</h1>
+                <div className={style.projects}>
+                    <div className={style.project}>포트폴리오</div>
+                    <div className={style.project}>게시판</div>
+                    <div className={style.project}>todo list</div>
+                    <div className={style.project}>chat</div>
+                    <div className={style.project}>싸피 프로젝트 1</div>
+                    <div className={style.project}>싸피 프로젝트 2</div>
+                </div>
+            </div>
         </div>
     );
 };
