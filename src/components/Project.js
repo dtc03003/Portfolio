75f09916bb697a8a28a831bc28de721104edeec7
김동현 <dtc03003@naver.com>
2023-07-18 22:45:29 +0900

    모달 스타일 분리

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,23 @@ import style from "./../styles/Project.module.css";
 import React, { useState } from "react";
 import Modal from "react-modal";
 
+const ModalStyle = {
+    content: {
+        position: "absolute",
+        top: "25vh",
+        left: "25vw",
+        right: "25vw",
+        bottom: "25vh",
+        border: "1px solid #ccc",
+        background: "#fff",
+        overflow: "auto",
+        WebkitOverflowScrolling: "touch",
+        borderRadius: "4px",
+        outline: "none",
+        padding: "20px",
+    },
+};
+
 const Project = () => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -25,30 +42,7 @@ const Project = () => {
                                 setModalIsOpen(false);
                                 event.stopPropagation(); // 이벤트 버블링 방지
                             }}
-                            style={{
-                                overlay: {
-                                    position: "fixed",
-                                    top: 0,
-                                    left: 0,
-                                    right: 0,
-                                    bottom: 0,
-                                    backgroundColor: "rgba(255, 255, 255, 0.75)",
-                                },
-                                content: {
-                                    position: "absolute",
-                                    top: "25vh",
-                                    left: "25vw",
-                                    right: "25vw",
-                                    bottom: "25vh",
-                                    border: "1px solid #ccc",
-                                    background: "#fff",
-                                    overflow: "auto",
-                                    WebkitOverflowScrolling: "touch",
-                                    borderRadius: "4px",
-                                    outline: "none",
-                                    padding: "20px",
-                                },
-                            }}
+                            style={ModalStyle}
                         >
                             포트폴리오 더 보기 모달창입니다.
                         </Modal>
