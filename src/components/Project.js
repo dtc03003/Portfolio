2701fe5a1764a9c111fb50309fcd29d9c53d9fb9
김동현 <dtc03003@naver.com>
2023-07-18 22:03:14 +0900

    모달창 구현

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,51 @@
 /* css import */
 import style from "./../styles/Project.module.css";
+import React, { useState } from "react";
+import Modal from "react-modal";
 
 const Project = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
     return (
         <div className={style.Project}>
             <div className={style.inner}>
                 <h1>My Project</h1>
                 <div className={style.projects}>
-                    <div className={style.project}>포트폴리오</div>
+                    <div className={style.project}>
+                        <button onClick={() => setModalIsOpen(true)}>Modal Open</button>
+                        포트폴리오
+                        <Modal
+                            isOpen={modalIsOpen}
+                            ariaHideApp={false}
+                            onRequestClose={() => setModalIsOpen(false)}
+                            style={{
+                                overlay: {
+                                    position: "fixed",
+                                    top: 0,
+                                    left: 0,
+                                    right: 0,
+                                    bottom: 0,
+                                    backgroundColor: "rgba(255, 255, 255, 0.75)",
+                                },
+                                content: {
+                                    position: "absolute",
+                                    top: "25vh",
+                                    left: "25vw",
+                                    right: "25vw",
+                                    bottom: "25vh",
+                                    border: "1px solid #ccc",
+                                    background: "#fff",
+                                    overflow: "auto",
+                                    WebkitOverflowScrolling: "touch",
+                                    borderRadius: "4px",
+                                    outline: "none",
+                                    padding: "20px",
+                                },
+                            }}
+                        >
+                            This is Modal content
+                        </Modal>
+                    </div>
                     <div className={style.project}>게시판</div>
                     <div className={style.project}>todo list</div>
                     <div className={style.project}>chat</div>
