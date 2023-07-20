9d29bc79bfe0a52e47a4ff60f1545e69ecd2b050
김동현 <dtc03003@naver.com>
2023-07-20 17:22:15 +0900

    모달 컴포넌트화

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,52 +1,14 @@
 /* css import */
 import style from "./../styles/Project.module.css";
-import React, { useState } from "react";
-import Modal from "react-modal";
-
-const ModalStyle = {
-    content: {
-        position: "absolute",
-        top: "25vh",
-        left: "25vw",
-        right: "25vw",
-        bottom: "25vh",
-        border: "1px solid #ccc",
-        background: "#fff",
-        overflow: "auto",
-        WebkitOverflowScrolling: "touch",
-        borderRadius: "4px",
-        outline: "none",
-        padding: "20px",
-    },
-};
+import Modal1 from "./Modal/Modal1";
 
 const Project = () => {
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-
     return (
         <div className={style.Project}>
             <div className={style.inner}>
                 <h1>My Project</h1>
                 <div className={style.projects}>
-                    <div
-                        className={style.project}
-                        onClick={() => {
-                            setModalIsOpen(true);
-                        }}
-                    >
-                        포트폴리오
-                        <Modal
-                            isOpen={modalIsOpen}
-                            ariaHideApp={false}
-                            onRequestClose={(event) => {
-                                setModalIsOpen(false);
-                                event.stopPropagation(); // 이벤트 버블링 방지
-                            }}
-                            style={ModalStyle}
-                        >
-                            포트폴리오 더 보기 모달창입니다.
-                        </Modal>
-                    </div>
+                    <Modal1></Modal1>
                     <div className={style.project}>게시판</div>
                     <div className={style.project}>todo list</div>
                     <div className={style.project}>chat</div>
---
+++ src/styles/ModalStyle.js
@@ -0,0 +1,18 @@
+const ModalStyle = {
+    content: {
+        position: "absolute",
+        top: "25vh",
+        left: "25vw",
+        right: "25vw",
+        bottom: "25vh",
+        border: "1px solid #ccc",
+        background: "#fff",
+        overflow: "auto",
+        WebkitOverflowScrolling: "touch",
+        borderRadius: "4px",
+        outline: "none",
+        padding: "20px",
+    },
+};
+
+export default ModalStyle;
