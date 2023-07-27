06c98a49e9977a450f36b3865260e92df9bbd32f
김동현 <dtc03003@naver.com>
2023-07-27 22:24:31 +0900

    위치 조정

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,47 +4,52 @@ import style from "./../styles/Header.module.css";
 const Header = () => {
     return (
         <div className={style.Header}>
-            <div className={style.btn_items}>
-                <p
-                    className={style.btn_item}
-                    onClick={() => {
-                        scroll("Main");
-                    }}
-                >
-                    Home
-                </p>
-                <p
-                    className={style.btn_item}
-                    onClick={() => {
-                        scroll("About");
-                    }}
-                >
-                    About
-                </p>
-                <p
-                    className={style.btn_item}
-                    onClick={() => {
-                        scroll("Skills");
-                    }}
-                >
-                    Skill
-                </p>
-                <p
-                    className={style.btn_item}
-                    onClick={() => {
-                        scroll("Project");
-                    }}
-                >
-                    Project
-                </p>
-                <p
-                    className={style.btn_item}
-                    onClick={() => {
-                        scroll("Contact");
-                    }}
-                >
-                    Contact
-                </p>
+            <div className={style.inner}>
+                <div className={style.title}>
+                    <p>DH's Portfolio</p>
+                </div>
+                <div className={style.btn_items}>
+                    <p
+                        className={style.btn_item}
+                        onClick={() => {
+                            scroll("Main");
+                        }}
+                    >
+                        Home
+                    </p>
+                    <p
+                        className={style.btn_item}
+                        onClick={() => {
+                            scroll("About");
+                        }}
+                    >
+                        About
+                    </p>
+                    <p
+                        className={style.btn_item}
+                        onClick={() => {
+                            scroll("Skills");
+                        }}
+                    >
+                        Skill
+                    </p>
+                    <p
+                        className={style.btn_item}
+                        onClick={() => {
+                            scroll("Project");
+                        }}
+                    >
+                        Project
+                    </p>
+                    <p
+                        className={style.btn_item}
+                        onClick={() => {
+                            scroll("Contact");
+                        }}
+                    >
+                        Contact
+                    </p>
+                </div>
             </div>
         </div>
     );
