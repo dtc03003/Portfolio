01e0602cd77e81a27bb9eb19139a240431341eef
김동현 <dtc03003@naver.com>
2023-08-17 17:58:33 +0900

    자기소개 초안 작성

--- src/components/About.js
+++ src/components/About.js
@@ -6,23 +6,30 @@ const About = () => {
         <div className={style.About} id="About">
             <div className={style.inner}>
                 <div className={style.about_me}>
-                    <div className={style.title}>
+                    {/* <div className={style.title}>
                         <h1>About Me</h1>
-                    </div>
+                    </div> */}
                     <div className={style.photo}></div>
                     <div className={style.char}>
                         <h1>김동현</h1>
                     </div>
-                    <div className={style.char}>
+                    <div className={style.birth}>
                         <h1>96.03.17</h1>
                     </div>
                 </div>
                 <div className={style.intro}>
                     <div className={style.title}>
-                        <h1>자기소개</h1>
+                        <h1>About Me</h1>
                     </div>
                     <div className={style.info}>
-                        <h1>대충 자기소개하는 내용</h1>
+                        <h1>배우는 개발자</h1>
+                        <p>
+                            학부시절 아두이노를 이용한 프로젝트를 수행하며 개발에 대해 재미를
+                            느꼈습니다.
+                            <br /> SSAFY에서 팀원들과 함께 프로젝트들을 완성하며 협동심을
+                            길렀습니다.
+                            <br /> 항상 배우고 기록하여 완성도 높은 코드를 만들겠습니다.
+                        </p>
                     </div>
                 </div>
             </div>
