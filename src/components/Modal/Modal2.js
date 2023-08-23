9d1601f99015aa64c5b3c2d1934eaae6c940795e
김동현 <dtc03003@naver.com>
2023-08-23 22:31:38 +0900

    게시판 프로젝트 작성

--- src/components/Modal/Modal2.js
+++ src/components/Modal/Modal2.js
@@ -64,38 +64,51 @@ const Modal1 = () => {
                                 <h1>게시판</h1>
                             </div>
                             <div className={style.inner_intro}>
-                                <div className={style.main_description}>게시판 설명</div>
+                                <div className={style.main_description}>
+                                    jwt을 이용한 로그인 및 회원가입, 웹 페에지의 기초라고 할 수 있는
+                                    게시판의 CRUD 기능을 구현해보고 amazon에서 서비스하는 EC2를
+                                    이용하여 배포, 도메인 구매 후 적용 등 프론트엔드와 백엔드 모두
+                                    구현해 웹개발의 다양한 측면을 경험할 수 있는 좋은 기회였습니다.
+                                </div>
                                 <div className={style.description}>
                                     <div className={style.projects_label}>주요 기능</div>
                                     <div className={style.projects_value}>
-                                        주요 기능 설명 주요 기능 설명 주요 기능 설명 주요 기능 설명
-                                        주요 기능 설명 주요 기능 설명 주요 기능 설명 주요 기능 설명
-                                        주요 기능 설명 주요 기능 설명 주요 기능 설명 주요 기능 설명
-                                        주요 기능 설명
+                                        회원가입, 로그인, 글 등록, 수정, 삭제, 태그 기능
                                     </div>
                                 </div>
                                 <div className={style.description}>
                                     <div className={style.projects_label}>FrontEnd</div>
-                                    <div className={style.projects_value}>기능 설명</div>
+                                    <div className={style.projects_value}>React, Redux, jQuery</div>
                                 </div>
                                 <div className={style.description}>
                                     <div className={style.projects_label}>BackEnd</div>
-                                    <div className={style.projects_value}>기능 설명</div>
+                                    <div className={style.projects_value}>Node.js, Koa</div>
                                 </div>
                                 <div className={style.description}>
                                     <div className={style.projects_label}>DataBase</div>
-                                    <div className={style.projects_value}>기능 설명</div>
+                                    <div className={style.projects_value}>MongoDB</div>
                                 </div>
                                 <div className={style.description}>
                                     <div className={style.projects_label}>Deployment</div>
-                                    <div className={style.projects_value}>기능 설명</div>
+                                    <div className={style.projects_value}>AWS(EC2)</div>
+                                </div>
+                                <div className={style.description}>
+                                    <div className={style.projects_label}>GitHub</div>
+                                    <div className={style.projects_value}>
+                                        <a href="https://github.com/dtc03003/blog">
+                                            https://github.com/dtc03003/blog
+                                        </a>
+                                    </div>
                                 </div>
                                 <div className={style.description}>
                                     <div className={style.projects_label}>URL</div>
-                                    <div className={style.projects_value}>기능 설명</div>
+                                    <div className={style.projects_value}>
+                                        <a href="http://meerkatstudy.store/">
+                                            http://meerkatstudy.store/
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
-                            <div className={style.link}>깃허브 주소</div>
                         </div>
                     </div>
                 </div>
