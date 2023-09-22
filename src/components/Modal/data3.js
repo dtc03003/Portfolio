bd59991d8475ede10a432c4bb9559055d10cbd78
김동현 <dtc03003@naver.com>
2023-09-22 21:54:38 +0900

    이미지 업로드

---
+++ src/components/Modal/data3.js
@@ -0,0 +1,44 @@
+import img1 from "../../img/핏메이트1.png";
+import img2 from "../../img/핏메이트2.png";
+import img3 from "../../img/핏메이트3.png";
+import img4 from "../../img/핏메이트4.png";
+import img5 from "../../img/핏메이트5.png";
+import img6 from "../../img/핏메이트6.png";
+
+const imageData = [
+    {
+        label: "Image 1",
+        alt: "image1",
+        url: img1,
+    },
+
+    {
+        label: "Image 2",
+        alt: "image2",
+        url: img2,
+    },
+
+    {
+        label: "Image 3",
+        alt: "image3",
+        url: img3,
+    },
+
+    {
+        label: "Image 4",
+        alt: "image4",
+        url: img6,
+    },
+    {
+        label: "Image 5",
+        alt: "image5",
+        url: img5,
+    },
+    {
+        label: "Image 6",
+        alt: "image6",
+        url: img4,
+    },
+];
+
+export default imageData;
---
+++ src/components/Modal/data4.js
@@ -0,0 +1,38 @@
+import img1 from "../../img/S뱅크1.png";
+import img2 from "../../img/S뱅크2.png";
+import img3 from "../../img/S뱅크3.png";
+import img4 from "../../img/S뱅크4.png";
+import img5 from "../../img/S뱅크5.png";
+
+const imageData = [
+    {
+        label: "Image 1",
+        alt: "image1",
+        url: img1,
+    },
+
+    {
+        label: "Image 2",
+        alt: "image2",
+        url: img2,
+    },
+
+    {
+        label: "Image 3",
+        alt: "image3",
+        url: img3,
+    },
+
+    {
+        label: "Image 4",
+        alt: "image4",
+        url: img4,
+    },
+    {
+        label: "Image 5",
+        alt: "image5",
+        url: img5,
+    },
+];
+
+export default imageData;
