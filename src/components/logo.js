fbe1426ef5d58aa73617fc4747381b8c070f26f5
김동현 <dtc03003@naver.com>
2023-09-25 23:21:07 +0900

    로고 스타일 변경

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,7 +2,14 @@
 import style from "./../styles/Skills.module.css";
 
 const Logo = (props) => {
-    return <div className={style.skill} style={{ backgroundImage: `url(${props.logo})` }}></div>;
+    return (
+        <div className={style.skill}>
+            <div
+                className={style.inner_skill}
+                style={{ backgroundImage: `url(${props.logo})` }}
+            ></div>
+        </div>
+    );
 };
 
 export default Logo;
